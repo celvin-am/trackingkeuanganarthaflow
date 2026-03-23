6091f778c719f850abf139b068926203dafaa4b6
celvinandramaulana <celvin2103fv.2023@student.uny.ac.id>
2026-03-24 01:04:28 +0700

    debug: error login malah masuk gajelas auts prefix

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,7 +1,13 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
+  // Pastiin baseURL ini nembak ke domain API lo
   baseURL: import.meta.env.VITE_API_URL?.replace('/api', '') || "http://localhost:3000",
+
+  // 🔥 TAMBAHKAN INI (HARUS SAMA PERSIS DENGAN BACKEND)
+  advanced: {
+    cookiePrefix: "arthaflow"
+  }
 });
 
 export const { useSession, signIn, signUp, signOut } = authClient;
